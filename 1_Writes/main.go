f600edb28ed535c2a4f8acbea6db8148e1524a6c
Александр <akonovalov.dev@gmail.com>
2023-05-29 21:56:44 +0300

    Fixes writes

--- 1_Writes/main.go
+++ 1_Writes/main.go
@@ -7,13 +7,14 @@ import (
 
 func main() {
 	writes := 1000
-	var storage map[int]int
+	//var storage map[int]int
 
+	storage := make(map[int]int, writes)
 	wg := sync.WaitGroup{}
 	wg.Add(writes)
 	for i := 0; i < writes; i++ {
 		i := i
-		go func() {
+		go func() { //хэш таблицей нельзя пользоваться конкурентно
 			defer wg.Done()
 			storage[i] = i
 		}()
