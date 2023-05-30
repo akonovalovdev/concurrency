0ba93cf3b7587ce0a56b1c88956107817bf7970e
Александр <akonovalov.dev@gmail.com>
2023-05-30 23:50:57 +0300

    Add new directory once with map

---
+++ 3_once_with_map/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"math/rand"
+	"sync"
+)
+
+func main() {
+	alreadyStored := make(map[int]struct{})
+	mu := sync.Mutex{}
+	capacity := 1000
+
+	doubles := make([]int, 0, capacity)
+	for i := 0; i < capacity; i++ {
+		doubles = append(doubles, rand.Intn(10))  // create rand num 0...9
+	}
+
+	uniqueIDs := make (chan int, capacity)
+	wg := sync.WaitGroup{}
+
+
+	for i := 0
+}
