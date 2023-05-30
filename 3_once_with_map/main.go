4af930fc18c589523fcc9dc3ed6884b38e2371eb
Александр <akonovalov.dev@gmail.com>
2023-05-30 23:55:33 +0300

    Fises once with map

--- 3_once_with_map/main.go
+++ 3_once_with_map/main.go
@@ -12,12 +12,17 @@ func main() {
 
 	doubles := make([]int, 0, capacity)
 	for i := 0; i < capacity; i++ {
-		doubles = append(doubles, rand.Intn(10))  // create rand num 0...9
+		doubles = append(doubles, rand.Intn(10)) // create rand num 0...9
 	}
 
-	uniqueIDs := make (chan int, capacity)
+	uniqueIDs := make(chan int, capacity)
 	wg := sync.WaitGroup{}
 
+	for i := 0; i < capacity; i++ {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
 
-	for i := 0
+		}
+	}
 }
