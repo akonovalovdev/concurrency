1a5dc457b36ea2a9a9266c96a8638e89eee681af
Александр <akonovalov.dev@gmail.com>
2023-05-31 00:00:54 +0300

    Fises 2 once with map

--- 3_once_with_map/main.go
+++ 3_once_with_map/main.go
@@ -22,7 +22,12 @@ func main() {
 		wg.Add(1)
 		go func() {
 			defer wg.Done()
-
-		}
+			if _, ok := aleradyStored[doubles[i]]; !ok {
+				mu.Lock()
+				alreadyStored[doubles[i]] = struct{}{}
+				mu.Unlock()
+				uniqueIDs <- doubles[i]
+			}
+		}()
 	}
 }
