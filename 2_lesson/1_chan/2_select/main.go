dcf4fff399acd1d0a8857f6f076e725d4418cafd
Александр <akonovalov.dev@gmail.com>
2023-05-31 22:37:11 +0300

    Fixes directory lesson2/select

--- 2_lesson/1_chan/2_select/main.go
+++ 2_lesson/1_chan/2_select/main.go
@@ -1,2 +1,24 @@
-//задача про тайм аут
+// задача про тайм аут
 package main
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+func main() {
+	rand.Seed(time.Now().Unix())
+	chanForResp := make(chan int)
+	go RPCCall(chanForResp)
+
+	result := <-chanForResp
+	fmt.Println(result)
+}
+
+func RPCCall(ch chan<- int) {
+	// sleep 0-4 sec
+	time.Sleep(time.Second * time.Duration(rand.Intn(5)))
+
+	ch <- rand.Int()
+}
