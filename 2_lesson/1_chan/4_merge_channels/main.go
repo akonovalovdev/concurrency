653a390abed71b1940ae8c9963e93acd45943f15
Александр <akonovalov.dev@gmail.com>
2023-06-02 22:02:53 +0300

    Fixes directory lesson2/merge_channels

--- 2_lesson/1_chan/4_merge_channels/main.go
+++ 2_lesson/1_chan/4_merge_channels/main.go
@@ -12,29 +12,40 @@ func main() {
 	close(ch1)
 	close(ch2)
 
-	ch3 := syncMerge[int](ch1, ch2)
+	ch3 := merge[int](ch1, ch2)
 
 	for val := range ch3 {
 		fmt.Println(val)
 	}
 }
 
-//func merge[T any](chans ...chan T) chan T {
-//	return nil
-//}
+func merge[T any](chans ...chan T) chan T {
+	result := make(chan T)
 
-func syncMerge[T any](chans ...chan T) chan T { // синхронная функция
-	l := 0
-	for _, singleCh := range chans {
-		l += len(singleCh)
+	for _, singleChan := range chans {
+		singleChan := singleChan
+		go func() {
+			for val := range singleChan {
+				result <- val
+			}
+		}()
 	}
-	result := make(chan T, l)
-	for _, singleCh := range chans {
-		for val := range singleCh {
-			result <- val
-		}
-	}
-	close(result)
 
-	return result
+	return nil
 }
+
+//func syncMerge[T any](chans ...chan T) chan T { // синхронная функция
+//	l := 0
+//	for _, singleCh := range chans {
+//		l += len(singleCh)
+//	}
+//	result := make(chan T, l) // буфер необходим так как функция синхронная
+//	for _, singleCh := range chans {
+//		for val := range singleCh {
+//			result <- val
+//		}
+//	}
+//	close(result)
+//
+//	return result
+//}
